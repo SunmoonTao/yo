436620f5b969e9050f92a7b053cf52c9180aa1d1
Leeming Tao <taoliming.too@gmail.com>
2019-10-04 23:32:12 -0400

    add myfunc module

--- tao/myfunc.py
+++ tao/myfunc.py
@@ -1,15 +1,17 @@
-
 from Bio.Seq import Seq
 import pandas as pd
 from collections import defaultdict
 
+
 def rc(str_seq):
     return str(Seq(str_seq).reverse_complement())
+
 
 def xls2dic(file_path,sheetname):
     idh4x = pd.read_excel(file_path, sheet_name="Sheet1")
     idh4x=idh4x.to_dict(orient="index")
     return idh4x
+
 
 def replace_str_index(text,index=0,replacement=''):
     return text[:index]+replacement+text[index+1:] 
@@ -26,7 +28,6 @@ def srandom_mutate(dna):
 # input a string of dna seq, return a list of mutated seqs of this dna with one mutated locus
 
 def mrandom_mutate(dna,mutation_numbers=1):
-
     muts=defaultdict(set)
     for r in range(mutation_numbers+1):
         mutations=['A',"T",'C','G']
@@ -49,7 +50,6 @@ def random_mutate(dna):
     return set(out)
 
 def mrandom_mutate(dna,mutation_numbers=1):
-
     muts=defaultdict(set)
     for r in range(mutation_numbers+1):
         mutations=['A',"T",'C','G']
