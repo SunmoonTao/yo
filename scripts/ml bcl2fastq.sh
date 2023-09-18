799e7da5422a3a160f432bff2fa81b855f05e7ca
SunmoonTao <taoliming.too@gmail.com>
2023-09-18 16:24:39 -0700

    sync

---
+++ scripts/ml bcl2fastq.sh
@@ -0,0 +1,3 @@
+ml bcl2fastq
+
+bcl2fastq  --no-lane-splitting --runfolder-dir /gnet/ais1/p01/shares/CTG/SeqData/Bia/runs/230511_VH00713_61_AAAYT7LHV  --minimum-trimmed-read-length 25 --output-dir   ./ --sample-sheet ./hct116R2.csv
